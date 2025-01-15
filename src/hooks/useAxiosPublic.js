986e23e29c0e5c392eb79de92cd7b8ecaea969e6
Jhalak <stdjhalak@gmail.com>
2025-01-15 12:08:56 +0600

    Final

--- src/hooks/useAxiosPublic.js
+++ src/hooks/useAxiosPublic.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosPublic=axios.create({
-       baseURL:'https://delta-dev-server.vercel.app',
+       baseURL:'https://delta-dev-stdjhalakgmailcoms-projects.vercel.app',
        
 })
 
